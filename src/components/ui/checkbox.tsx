f4ecb8be47a02b83d68f153fa37fd7ec253f3ae4
Fischimbiss <bjoernf@t-online.de>
2025-08-24 13:09:29 +0200

    Create checkbox.tsx

---
+++ src/components/ui/checkbox.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+type Props = React.InputHTMLAttributes<HTMLInputElement>
+
+export const Checkbox = React.forwardRef<HTMLInputElement, Props>(({ className, ...p }, ref) => (
+  <input
+    ref={ref}
+    type="checkbox"
+    className={['h-4 w-4 rounded border-neutral-500 bg-neutral-900', className || ''].join(' ')}
+    {...p}
+  />
+))
+Checkbox.displayName = 'Checkbox'
