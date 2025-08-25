d80d39785a7ad5e80a02a662e17f382542459574
Fischimbiss <bjoernf@t-online.de>
2025-08-25 15:55:06 +0200

    Update input.tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -5,7 +5,7 @@ export const Input = React.forwardRef<HTMLInputElement, Props>(({ className, ...
   <input
     ref={ref}
     className={[
-      'w-full rounded-md border border-neutral-600 bg-neutral-900 px-3 py-2 text-sm',
+      'w-full rounded-md border border-neutral-200 bg-neutral-100 px-3 py-2 text-sm',
       'placeholder:text-neutral-400 focus:outline-none focus:ring-2 focus:ring-blue-600',
       className || ''
     ].join(' ')}
